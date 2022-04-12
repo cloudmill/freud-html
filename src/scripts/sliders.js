fcd9f3c07302189729b0b99bbd2ce04f20e8c0ba
Kirill Matveev <dejar@protonmail.com>
2022-04-12 15:18:28 +0300

    worked on the main page

--- src/scripts/sliders.js
+++ src/scripts/sliders.js
@@ -1,7 +1,5 @@
 import Swiper, { Navigation, Pagination } from 'swiper';
 
-
-
 window.addEventListener("DOMContentLoaded", () => {
   const swiper = new Swiper("#slider1", {
 
@@ -10,8 +8,31 @@ window.addEventListener("DOMContentLoaded", () => {
     loop: true,
 
     navigation: {
-      nextEl: "#swiper-button-next",
-      prevEl: "#swiper-button-prev",
+      nextEl: "#restaurant-slider-next",
+      prevEl: "#restaurant-slider-prev",
+    },
+
+    pagination: {
+      el: "#restaurant-slider-pagination",
+    },
+  });
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const swiper = new Swiper("#reviews-slider", {
+
+    modules: [Navigation, Pagination],
+
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+
+    navigation: {
+      nextEl: "#reviews-slider-next",
+      prevEl: "#reviews-slider-prev",
+    },
+
+    pagination: {
+      el: "#reviews-slider-pagination",
     },
   });
 });
