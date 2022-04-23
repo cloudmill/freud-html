f9731f9424ce279a277eb4bb9952c51a85bdef95
kirill matveev <dejar@protonmail.com>
2022-04-23 15:23:07 +0300

    added mediaQueries and header scripts

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import './styles/app.scss';
 
+import './scripts/mediaQueries';
 import './scripts/sliders';
 import './scripts/spoiler';
 import './scripts/tabs';
 import './scripts/form-validation';
+import './scripts/header';
