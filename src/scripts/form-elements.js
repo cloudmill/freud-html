1ffc3286253ee6448c9c0a92b1a8c1cec0462e7a
kirill matveev <dejar@protonmail.com>
2022-05-03 23:48:18 +0300

    turned on the datepicker

--- src/scripts/form-elements.js
+++ src/scripts/form-elements.js
@@ -5,21 +5,21 @@ Object.assign(Datepicker.locales, ru);
 
 // datepicker
 
-// const datepickerEl = document.querySelector('[data-datepicker]');
+const datepickerEl = document.querySelector('[data-datepicker]');
 
-// new Datepicker(datepickerEl, {
-//   format: 'dd/mm/yyyy',
-//   autohide: true,
-//   language: 'ru',
-//   orientation: 'top',
+new Datepicker(datepickerEl, {
+  format: 'dd/mm/yyyy',
+  autohide: true,
+  language: 'ru',
+  orientation: 'top',
 
-//   prevArrow: `<svg class="datepicker-arrow" width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-//   <path d="M6 11L1 6L6 1" stroke="#1B1B1B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-//   </svg>`,
-//   nextArrow: `<svg class="datepicker-arrow next" width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-//   <path d="M6 11L1 6L6 1" stroke="#1B1B1B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-//   </svg>`,
-// }); 
+  prevArrow: `<svg class="datepicker-arrow" width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M6 11L1 6L6 1" stroke="#1B1B1B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>`,
+  nextArrow: `<svg class="datepicker-arrow next" width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M6 11L1 6L6 1" stroke="#1B1B1B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>`,
+}); 
 
 // select
 
