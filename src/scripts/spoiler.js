182d703c0118339a935a1eb640c0d0592dfee9c3
Kirill Matveev <dejar@protonmail.com>
2022-05-04 12:10:22 +0300

    upd spoilers and bokking popup

--- src/scripts/spoiler.js
+++ src/scripts/spoiler.js
@@ -1,31 +1,4 @@
 import BadgerAccordion from 'badger-accordion';
 
-const accordions = document.querySelectorAll('.js-badger-accordion');
-
-Array.from(accordions).forEach((accordion) => {
-    const ba = new BadgerAccordion(accordion);
-});
-
-
-
-// window.addEventListener('DOMContentLoaded', () => {
-
-//   const spoilers = document.querySelectorAll('[data-spoiler]');
-
-//   if (spoilers) {
-    
-//     const buttons = document.querySelectorAll('[data-spoiler-btn]');
-
-//     buttons.forEach(item => {
-
-//       item.addEventListener('click', event => {
-//         console.log('click');
-
-//         const currentSpoiler = event.target.closest('[data-spoiler]');
-
-//         // spoilers.forEach(item => item.classList.remove('active'));
-//         currentSpoiler.classList.toggle('active');
-//       })
-//     })
-//   }
-// })
+var accordionDomNode = document.querySelector('.js-badger-accordion');
+var accordion = new BadgerAccordion(accordionDomNode);
