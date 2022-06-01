59147fe1786b2110f953f3e5f1a68b14f73c3652
Kirill Matveev <dejar@protonmail.com>
2022-06-01 13:40:08 +0300

    worked on the cart general

--- src/app.js
+++ src/app.js
@@ -124,23 +124,97 @@ document.addEventListener('DOMContentLoaded', () => {
 
 window.addEventListener('load', () => {
 
-  document.querySelectorAll('[data-cart-content]').forEach(item => {
+  // cart stages
 
-    console.log(item.offsetHeight);
+  if (document.querySelector('[data-cart-stage-general]')) {
 
-    const contentHeight = String(item.offsetHeight);
+    document.querySelectorAll('[data-cart-content]').forEach(item => {
 
-    item.style.height = contentHeight + 'px';
+      const contentHeight = String(item.offsetHeight);
+  
+      item.style.height = contentHeight + 'px';
+      
+    })
+  
+    document.querySelectorAll('.cart-stage.collapsed').forEach(item => {
+      item.querySelector('[data-cart-content]').classList.add('hide-content');
+  
+    })
     
+    document.querySelectorAll('[data-cart-next]').forEach(item => {
   
-  })
+      item.addEventListener('click', e => {
   
-  document.querySelector('[data-cart-next]').addEventListener('click', e => {
+        setTimeout(() => {
   
-    e.target.closest('[data-cart-stage]').classList.remove('open');
-    e.target.closest('[data-cart-stage]').classList.add('collapsed');
+          if (!Boolean(item.closest('form')) || item.closest('form') && !item.closest('form').querySelector('.form-error.active')) {
   
-  })
+            e.target.closest('[data-cart-stage]').classList.remove('open');
+            e.target.closest('[data-cart-stage]').classList.add('previous');
+    
+            const btnId = item.getAttribute('data-cart-next');
+            
+            document.querySelector(`[data-cart-stage='${btnId}']`).classList.remove('previous');
+            document.querySelector(`[data-cart-stage='${btnId}']`).classList.remove('collapsed');
+            document.querySelector(`[data-cart-stage='${btnId}']`).classList.add('open');
+    
+            if (!mediaQuery.matches) {
+    
+              e.target.closest('[data-cart-stage]').scrollIntoView({
+                block: 'start'
+              })  
+            } 
+          }
+        }, );
+      })
+    })
+  
+    document.querySelectorAll('[data-stage-edit]').forEach(item => {
+  
+      item.addEventListener('click', e => {
+  
+        document.querySelectorAll('[data-cart-stage]').forEach(item => {
+          item.classList.remove('open');
+          item.classList.add('collapsed');
+        })
+  
+        e.target.closest('[data-cart-stage]').classList.remove('collapsed');
+        e.target.closest('[data-cart-stage]').classList.remove('previous');
+        e.target.closest('[data-cart-stage]').classList.add('open');
+  
+      })
+    })
+  
+    document.querySelector('[data-cart-big-form]').addEventListener('click', e => {
+      e.target.closest('.cart-receiving').classList.add('height-auto')
+    });
+    document.querySelector('[data-cart-small-form]').addEventListener('click', e => {
+      e.target.closest('.cart-receiving').classList.remove('height-auto')
+    })
+  
+    document.querySelectorAll('[data-cart-final]').forEach(item => {
+  
+      item.addEventListener('click', e => {
+
+        setTimeout(() => {
+
+          console.log(item.closest('form').querySelector('.form-error.active'));
+          
+          if (!Boolean(item.closest('form')) || item.closest('form') && !item.closest('form').querySelector('.form-error.active')) {
+  
+            document.querySelector('[data-cart-body-stages]').classList.add('hide');
+            document.querySelector('[data-cart-body-result]').classList.remove('hide');
+            // document.querySelector('[data-cart-body-result]').classList.add('show');
+    
+          }
+
+        }, );
+      })
+
+    })
+  }
+
+  // 
 
   document.querySelector('body').classList.remove('no-transition');
 
