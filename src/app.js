1e01c7977eb041bf37b7cbb2e2e8d026d2d18860
Kirill Matveev <dejar@protonmail.com>
2022-06-08 16:06:49 +0300

    added search popup

--- src/app.js
+++ src/app.js
@@ -77,6 +77,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
     }
 
+    // модалка поиска на мобилке
+    
+    if (eventClick.target.closest('.mobile-nav__item').hasAttribute('data-popup-button')) {
+
+      eventClick.target.closest('.mobile-nav__item').classList.add('active');
+      eventClick.target.closest('.mobile-nav').classList.add('index-raise');
+
+      eventClick.target.closest('.mobile-nav__item').removeAttribute('data-popup-button');
+      eventClick.target.closest('.mobile-nav__item').setAttribute('data-modal-close', 'data-modal-close');
+
+    } else if (eventClick.target.closest('.mobile-nav__item').hasAttribute('data-modal-close')) {
+
+      eventClick.target.closest('.mobile-nav__item').removeAttribute('data-modal-close');
+      eventClick.target.closest('.mobile-nav__item').setAttribute('data-popup-button', '14');
+
+      eventClick.target.closest('.mobile-nav__item').classList.remove('active');
+
+      setTimeout(() => {
+        eventClick.target.closest('.mobile-nav').classList.remove('index-raise');
+      }, 300);
+
+    }
+
   });
 
   if (mediaQuery.matches) {
