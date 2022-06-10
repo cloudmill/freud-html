abb4d8cf33b233e0470f47b4cffd810a2f355c29
Kirill Matveev <dejar@protonmail.com>
2022-06-10 12:11:41 +0300

    tablet fixes upd dev server script

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -27,6 +27,10 @@ module.exports = (env) => {
     devServer: {
       contentBase: path.resolve(__dirname, "../src"),
       openPage: "index",
+      host: '0.0.0.0',
+      port: '8000',
+      hot: true,
+      open: true,
     },
     devtool: env.NODE_ENV === "development" ? "source-map" : false,
     resolve: {
