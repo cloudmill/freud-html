b55eb4199cf4028689e4a2ebd5b636a6ade337bc
Kirill Matveev <dejar@protonmail.com>
2022-06-28 18:38:22 +0300

    update humidor slider

--- src/scripts/slider-humidor.js
+++ src/scripts/slider-humidor.js
@@ -2,104 +2,22 @@ import { mediaQuery } from './mediaQueries';
 
 function humidorSlider() {
 
-  const humidorOverlay = document.querySelector('.humidor-slider-overlay');
-  const lastSlide = document.querySelector('.last-slide');
-  const firstSlide = document.querySelector('.first-slide');
-  const humidorSlider = document.querySelector('.humidor-slider');
+  if (mediaQuery.matches) {
 
-  if (humidorOverlay && mediaQuery.matches) {
+    const section = document.querySelector('.humidor-slider2-section');
+    const slider = section.querySelector('.humidor-slider2-slider');
+    const container = document.querySelector('.humidor-slider2-container');
 
-    const headerHeight = document.querySelector('.header').offsetHeight;
-    const sliderHeight = humidorSlider.offsetHeight;
-    const windowHeight = document.documentElement.clientHeight;
-    const topOffset = (windowHeight - sliderHeight) / 2;
+    container.style.top = `${(document.documentElement.clientHeight / 2) - (container.offsetHeight / 2)}px`
 
-    window.addEventListener('scroll', () => {
+    if (section) {
+      addEventListener('scroll', () => {
+        const progress = Math.min(Math.max(0, -(section.getBoundingClientRect().top - innerHeight / 4)) / (section.getBoundingClientRect().height - innerHeight / 4 * 3), 1);
 
-      if (humidorSlider.getBoundingClientRect().top < topOffset && 
-      humidorSlider.getBoundingClientRect().top > 0 &&
-          !humidorOverlay.classList.contains('start-slider')) {
-
-        document.querySelector('body').classList.add('fix-scroll');
-        humidorOverlay.classList.add('active');
-
-        humidorOverlay.classList.add('start-slider');
-
-      } else if (humidorSlider.getBoundingClientRect().top < topOffset && 
-                humidorSlider.getBoundingClientRect().top > topOffset - 20 &&
-                humidorOverlay.classList.contains('at-last')) {
-
-        document.querySelector('body').classList.add('fix-scroll');
-        humidorOverlay.classList.add('active');
-        
-      }
-    });
-
-    const observerLastSlide = new IntersectionObserver(callbackLastSlide, {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.95,
-    });
-
-    observerLastSlide.observe(lastSlide);
-
-
-    const observerFirstSlide = new IntersectionObserver(callbackFirstSlide, {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.95,
-    });
-
-    observerFirstSlide.observe(firstSlide);
-
-
-
-
-    function callbackLastSlide(entries, observer) {
-
-      if (humidorOverlay.getBoundingClientRect().top < 100 && !humidorOverlay.classList.contains('at-last')) {
-  
-        document.querySelector('body').classList.remove('fix-scroll');
-        humidorOverlay.classList.remove('active');
-  
-        document.addEventListener('scroll', addLastClass)
-      }
-      
-    }
-  
-    function callbackFirstSlide(entries, observer) {
-  
-      if (humidorOverlay.getBoundingClientRect().top < topOffset && humidorOverlay.classList.contains('at-last')) {
-  
-        document.querySelector('body').classList.remove('fix-scroll');
-        humidorOverlay.classList.remove('active');
-        humidorOverlay.classList.remove('at-last');
-  
-        document.addEventListener('scroll', removeFirstClass)
-  
-      }
-    }
-  
-    function addLastClass() {
-      if (humidorOverlay.getBoundingClientRect().top < 0) {
-  
-        humidorOverlay.classList.add('at-last');
-        document.removeEventListener('scroll', addLastClass)
-      }
-    }
-  
-    function removeFirstClass() {
-      if (humidorOverlay.getBoundingClientRect().top > 200) {
-        humidorOverlay.classList.remove('start-slider');
-        document.removeEventListener('scroll', removeFirstClass)
-      }
-      
-    }
-    
+        slider.style.transform = `translateX(-${(slider.getBoundingClientRect().width - innerWidth) * progress}px)`;
+      });
+    } 
   }
-
-  
-  
 }
 
 export default humidorSlider;
