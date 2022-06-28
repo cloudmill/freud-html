d504199cdc5b1d6b77c9d3239639d5aad22cd302
Kirill Matveev <dejar@protonmail.com>
2022-06-28 15:52:20 +0300

    upd humidor slider

--- src/scripts/slider-humidor.js
+++ src/scripts/slider-humidor.js
@@ -5,18 +5,35 @@ function humidorSlider() {
   const humidorOverlay = document.querySelector('.humidor-slider-overlay');
   const lastSlide = document.querySelector('.last-slide');
   const firstSlide = document.querySelector('.first-slide');
+  const humidorSlider = document.querySelector('.humidor-slider');
 
   if (humidorOverlay && mediaQuery.matches) {
 
+    const headerHeight = document.querySelector('.header').offsetHeight;
+    const sliderHeight = humidorSlider.offsetHeight;
+    const windowHeight = document.documentElement.clientHeight;
+    const topOffset = (windowHeight - sliderHeight) / 2;
 
-    const observerOverlay = new IntersectionObserver(callbackOverlay, {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.98,
+    window.addEventListener('scroll', () => {
+
+      if (humidorSlider.getBoundingClientRect().top < topOffset && 
+      humidorSlider.getBoundingClientRect().top > 0 &&
+          !humidorOverlay.classList.contains('start-slider')) {
+
+        document.querySelector('body').classList.add('fix-scroll');
+        humidorOverlay.classList.add('active');
+
+        humidorOverlay.classList.add('start-slider');
+
+      } else if (humidorSlider.getBoundingClientRect().top < topOffset && 
+                humidorSlider.getBoundingClientRect().top > topOffset - 20 &&
+                humidorOverlay.classList.contains('at-last')) {
+
+        document.querySelector('body').classList.add('fix-scroll');
+        humidorOverlay.classList.add('active');
+        
+      }
     });
-
-    observerOverlay.observe(humidorOverlay);
-    
 
     const observerLastSlide = new IntersectionObserver(callbackLastSlide, {
       root: null,
@@ -34,74 +51,54 @@ function humidorSlider() {
     });
 
     observerFirstSlide.observe(firstSlide);
-    
-  }
 
-  function addLastClass() {
-    if (humidorOverlay.getBoundingClientRect().top < -600) {
-      console.log('last class');
 
-      humidorOverlay.classList.add('at-last');
-      document.removeEventListener('scroll', addLastClass)
+
+
+    function callbackLastSlide(entries, observer) {
+
+      if (humidorOverlay.getBoundingClientRect().top < 100 && !humidorOverlay.classList.contains('at-last')) {
+  
+        document.querySelector('body').classList.remove('fix-scroll');
+        humidorOverlay.classList.remove('active');
+  
+        document.addEventListener('scroll', addLastClass)
+      }
+      
     }
-  }
-
-  function callbackOverlay(entries, observer) {
-
-    console.log('callback overlay 1');
-    console.log(humidorOverlay.getBoundingClientRect().top < 0, !humidorOverlay.classList.contains('at-first'));
-
-    if (humidorOverlay.getBoundingClientRect().top < 0 && !humidorOverlay.classList.contains('at-first') || humidorOverlay.classList.contains('at-last')) {
-
-      console.log('callback overlay 2');
-
-      document.querySelector('body').classList.add('fix-scroll');
-      humidorOverlay.classList.add('active');
-
+  
+    function callbackFirstSlide(entries, observer) {
+  
+      if (humidorOverlay.getBoundingClientRect().top < topOffset && humidorOverlay.classList.contains('at-last')) {
+  
+        document.querySelector('body').classList.remove('fix-scroll');
+        humidorOverlay.classList.remove('active');
+        humidorOverlay.classList.remove('at-last');
+  
+        document.addEventListener('scroll', removeFirstClass)
+  
+      }
     }
-    
-  }
-
-  function callbackLastSlide(entries, observer) {
-
-    if (humidorOverlay.getBoundingClientRect().top < 100) {
-
-      console.log('callback last slide');
-
-      document.querySelector('body').classList.remove('fix-scroll');
-      humidorOverlay.classList.remove('active');
-
-      document.addEventListener('scroll', addLastClass)
+  
+    function addLastClass() {
+      if (humidorOverlay.getBoundingClientRect().top < 0) {
+  
+        humidorOverlay.classList.add('at-last');
+        document.removeEventListener('scroll', addLastClass)
+      }
     }
-    
-  }
-
-  function callbackFirstSlide(entries, observer) {
-
-    if (humidorOverlay.getBoundingClientRect().top < 200 && humidorOverlay.classList.contains('at-last')) {
-
-      console.log('callback first slide');
-
-      document.querySelector('body').classList.remove('fix-scroll');
-      humidorOverlay.classList.remove('active');
-
-      humidorOverlay.classList.add('at-first');
-      humidorOverlay.classList.remove('at-last');
-
-      document.addEventListener('scroll', removeFirstClass)
-
-    }
-  }
-
-  function removeFirstClass() {
-    if (humidorOverlay.getBoundingClientRect().top > 200) {
-      console.log('remove first class');
-
-      humidorOverlay.classList.remove('at-first');
-      document.removeEventListener('scroll', removeFirstClass)
+  
+    function removeFirstClass() {
+      if (humidorOverlay.getBoundingClientRect().top > 200) {
+        humidorOverlay.classList.remove('start-slider');
+        document.removeEventListener('scroll', removeFirstClass)
+      }
+      
     }
     
   }
+
+  
   
 }
 
