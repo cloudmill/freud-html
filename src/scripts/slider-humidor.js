37622e584a3b0412a35b2a0967dc57e34680f106
Kirill Matveev <dejar@protonmail.com>
2022-06-29 11:17:04 +0300

    added rules section to the club card page

--- src/scripts/slider-humidor.js
+++ src/scripts/slider-humidor.js
@@ -2,23 +2,21 @@ import { mediaQuery } from './mediaQueries';
 
 function humidorSlider() {
 
-  if (mediaQuery.matches) {
+  const section = document.querySelector('.humidor-slider2-section');
 
-    const section = document.querySelector('.humidor-slider2-section');
+  if (mediaQuery.matches && section) {
 
-    if (section) {
+    const slider = section.querySelector('.humidor-slider2-slider');
+    const container = document.querySelector('.humidor-slider2-container');
 
-      const slider = section.querySelector('.humidor-slider2-slider');
-      const container = document.querySelector('.humidor-slider2-container');
-  
-      container.style.top = `${(document.documentElement.clientHeight / 2) - (container.offsetHeight / 2)}px`;
+    container.style.top = `${(document.documentElement.clientHeight / 2) - (container.offsetHeight / 2)}px`;
 
-      addEventListener('scroll', () => {
-        const progress = Math.min(Math.max(0, -(section.getBoundingClientRect().top - innerHeight / 4)) / (section.getBoundingClientRect().height - innerHeight / 4 * 3), 1);
+    addEventListener('scroll', () => {
+      const progress = Math.min(Math.max(0, -(section.getBoundingClientRect().top - innerHeight / 4)) / (section.getBoundingClientRect().height - innerHeight / 4 * 3), 1);
 
-        slider.style.transform = `translateX(-${(slider.getBoundingClientRect().width - innerWidth) * progress}px)`;
-      });
-    } 
+      slider.style.transform = `translateX(-${(slider.getBoundingClientRect().width - innerWidth) * progress}px)`;
+    });
+    
   }
 }
 
