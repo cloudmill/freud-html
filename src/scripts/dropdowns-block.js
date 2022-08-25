e9e4700712da2aca8139f5f25ab25136d1c05721
kirill matveev <dejar@protonmail.com>
2022-08-25 16:46:06 +0300

    updates

--- src/scripts/dropdowns-block.js
+++ src/scripts/dropdowns-block.js
@@ -1,5 +1,5 @@
 import { openWindow, closeWindow } from './modals-open-close';
-import { focusOnTab, focusOnTabOff } from "./tabindex-in-modals";
+import dropOnHover from './drops-on-hover';
 
 function dropdownsBlock(trigger, btnSelector, windowSelector, blockScroll) {
   
@@ -14,77 +14,8 @@ function dropdownsBlock(trigger, btnSelector, windowSelector, blockScroll) {
     let clickOff;
 
     if (trigger.type == 'mouseover' && activeBtn) {
-  
-      if (blockScroll) {
-        document.querySelector('.body').classList.add('modal-open');
-      }
-      if (modalsContainer) {
-        modalsContainer.classList.add('active');
-      }
     
-      const activeId = Number(activeBtn.getAttribute(btnSelector));
-      const activeWindow = document.querySelector(`[${windowSelector}='${activeId}']`);
-
-      triggerBtns.forEach(item => {
-        if (item.getAttribute(btnSelector) !== activeId) {
-          item.classList.remove('active')
-        }
-      });
-      
-      windows.forEach(item => {
-
-        if (item.classList.contains('header-modals__item')) {
-
-          if (item !== activeWindow) {
-            item.style.zIndex = '4';
-  
-            setTimeout(() => {
-              item.classList.remove('active');
-              item.style.zIndex = '';
-            }, 300);
-            
-          }
-          
-        } else {
-
-          item.classList.remove('active');
-
-        }
-
-      });
-    
-      activeBtn.classList.add('active');
-      activeWindow.classList.add('active');
-    
-      if (activeWindow.querySelector('[data-focus-input]')) {
-        activeWindow.querySelector('[data-focus-input]').focus();
-      }
-    
-      focusOnTab(activeWindow);
-
-      document.querySelector('.header__top').addEventListener('mouseover', e => {
-        
-        if (e.relatedTarget == document.querySelector('.header__bottom')) {
-          closeWindow(triggerBtns, windows, blockScroll, modalsContainer);
-        }
-      });
-
-      document.querySelector('[data-drop-close]').addEventListener('mouseover', e => {
-        
-        closeWindow(triggerBtns, windows, blockScroll, modalsContainer);
-
-      });
-  
-      document.addEventListener('click', eventClick => {
-
-        clickOff = document.querySelector(`.active[${windowSelector}]`) && 
-        !eventClick.target.closest(`[${btnSelector}]`) && 
-        !eventClick.target.closest(`[${windowSelector}]`);
-  
-        if (clickOff) {
-          closeWindow(triggerBtns, windows, blockScroll, modalsContainer);
-        }
-      });
+      dropOnHover(activeBtn, btnSelector, windowSelector, triggerBtns, windows, blockScroll, modalsContainer, clickOff, trigger)
 
     } else if (trigger.type == 'click') {
 
---
+++ src/scripts/drops-on-hover.js
@@ -0,0 +1,79 @@
+import { closeWindow } from './modals-open-close';
+import { focusOnTab } from "./tabindex-in-modals";
+
+export default function dropOnHover(activeBtn, btnSelector, windowSelector, triggerBtns, windows, blockScroll, modalsContainer, clickOff, trigger) {
+
+  if (activeBtn.classList.contains('active')) {
+
+    console.log('close');
+    closeWindow(triggerBtns, windows, blockScroll, modalsContainer)
+    
+  } else {
+
+    if (blockScroll) {
+      document.querySelector('.body').classList.add('modal-open');
+    }
+    if (modalsContainer) {
+      modalsContainer.classList.add('active');
+    }
+
+    const activeId = Number(activeBtn.getAttribute(btnSelector));
+    const activeWindow = document.querySelector(`[${windowSelector}='${activeId}']`);
+
+    triggerBtns.forEach(item => {
+      if (item.getAttribute(btnSelector) !== activeId) {
+        item.classList.remove('active')
+      }
+    });
+    
+    windows.forEach(item => {
+
+      if (item.classList.contains('header-modals__item')) {
+
+        if (item !== activeWindow) {
+          item.style.zIndex = '4';
+
+          setTimeout(() => {
+            item.classList.remove('active');
+            item.style.zIndex = '';
+          }, 300); 
+        }
+      } else {
+        item.classList.remove('active');
+      }
+    });
+
+    activeBtn.classList.add('active');
+    activeWindow.classList.add('active');
+
+    if (activeWindow.querySelector('[data-focus-input]')) {
+      activeWindow.querySelector('[data-focus-input]').focus();
+    }
+
+    focusOnTab(activeWindow);
+
+    document.querySelector('.header__top').addEventListener('mouseover', e => {
+      
+      if (e.relatedTarget == document.querySelector('.header__bottom')) {
+        closeWindow(triggerBtns, windows, blockScroll, modalsContainer);
+      }
+    });
+
+    document.querySelector('[data-drop-close]').addEventListener('mouseover', e => {
+      
+      closeWindow(triggerBtns, windows, blockScroll, modalsContainer);
+
+    });
+
+    document.addEventListener('click', eventClick => {
+
+      clickOff = document.querySelector(`.active[${windowSelector}]`) && 
+      !eventClick.target.closest(`[${btnSelector}]`) && 
+      !eventClick.target.closest(`[${windowSelector}]`);
+
+      if (clickOff) {
+        closeWindow(triggerBtns, windows, blockScroll, modalsContainer);
+      }
+    });
+  }
+}
