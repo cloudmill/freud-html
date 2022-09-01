9916856b5b89e237fa9a5f36721169ee5470e0a6
kirill matveev <dejar@protonmail.com>
2022-09-01 17:46:53 +0300

    worked on the validation

--- src/scripts/form-elements/form-validation.js
+++ src/scripts/form-elements/form-validation.js
@@ -1,7 +1,8 @@
 function formValidation() {
   const forms = document.querySelectorAll('form');
+  let error;
 
-  if (forms) {
+  if (forms.length) {
 
     // на случай если форм на странице несколько
     forms.forEach(form => {
@@ -10,79 +11,59 @@ function formValidation() {
 
       inputs.forEach(item => {
 
-        let error;
-
         if (!item.hasAttribute('data-input-novalidate')) {
 
-          // добавляет элемент блок для текста ошибки в зависимости от верстки
-
-          if (item.getAttribute('type') == 'checkbox') {
-
-            item.closest('.checkbox').insertAdjacentHTML('beforeend', `<span class='form-error'></>`);
-            error = item.closest('.checkbox').querySelector('.form-error');
-            
-          } else if (item.closest('[data-select-input]')) {
-
-            item.closest('.select-label').insertAdjacentHTML('beforeend', `<span class='form-error'></>`);
-            error = item.closest('.select-label').querySelector('.form-error');
-
-          } else if (item.hasAttribute('data-datepicker')) {
-
-            item.closest('.form__label').insertAdjacentHTML('beforeend', `<span class='form-error'></>`);
-            error = item.closest('.form__label').querySelector('.form-error');
-
-          } else {
-            item.insertAdjacentHTML('afterend', `<span class='form-error'></>`)
-            error = item.nextElementSibling;
-          }
+          // добавляет блок для текста ошибки в зависимости от верстки
+          addingErrorBlock(item);
+          errorDefinition(item);
 
           // после потери курсора из инпута проводит валидацию
           // если есть ошибки - обрабатывает
           // если нет - обнуляет поле ошибки
 
-          item.addEventListener('focusout', (event) => {
+          item.addEventListener('focusout', () => {
       
-            if (item.validity.valid || item.hasAttribute('data-readonly') && !item.value.length || !(item.getAttribute('type') == 'tel' && chekTel(item))) {
-              // console.log('ok');
+            if (!item.validity.valid || 
+              item.hasAttribute('data-readonly') && !item.value.length || 
+              item.getAttribute('type') == 'tel' && chekTel(item)) {
       
-              error.textContent = '';
-              error.className = 'form-error';
+              showError(item, error);
       
             } else {
-              // console.log('ne ok');
     
-              showError(item, error);
+              error.textContent = '';
+              error.classList.remove('active');
             }
           });
-        }
+        }        
+      })
 
-        // при отправке формы повторная валидация
-        form.addEventListener('submit', function (event) {
+      // при отправке формы повторная валидация
+      form.addEventListener('submit', (event) => {
 
-          event.preventDefault();
-        
+        event.preventDefault();
+
+        inputs.forEach(item => {
+
+          errorDefinition(item);
+
+          // console.log(!item.validity.valid, item.getAttribute('type') == 'checkbox' && !item.checked, item.getAttribute('type') == 'tel' && chekTel(item));
+
           if (!item.validity.valid || 
             (item.getAttribute('type') == 'checkbox' && !item.checked) || 
             item.hasAttribute('data-readonly') && !item.value.length || 
-            item.closest('form').querySelector('.form-error.active') || 
-            (item.getAttribute('type') == 'tel' && chekTel(item))) {
-
+            item.getAttribute('type') == 'tel' && chekTel(item)) {
+  
             showError(item, error);
             
-
             console.log('form-error');
-
+  
           } else {
             form.querySelectorAll('.form-error.active').forEach(errors => {
               errors.classList.remove('active')
             })
           }
-        });
-        
-        
-      })
-
-      form.addEventListener('submit', (event) => {
+        })
 
         setTimeout(() => {
 
@@ -109,83 +90,120 @@ function formValidation() {
 
             }
           } 
-          
         }, );
-      })
+      });
     })
   }
-}
 
-// проверка телефона
-function chekTel(input) {
+  // проверка телефона
+  function chekTel(input) {
 
-  const value = input.value.replace(/\D/g, "");
+    if (input.getAttribute('type') == 'tel') {
 
-  console.log(value, !(value[0] === 7), !(value[0] === 8), value.length < 11);
+      console.log('ku');
 
-  if ((!(value[0] === 7) || !(value[0] === 8)) || value.length < 11) {
-    return true
+      const value = input.value.replace(/\D/g, "");
+
+      console.log(value[0] === 7, value[0] === 8, value.length < 10);
+
+      if ((value[0] === 7 || value[0] === 8) || value.length < 10) {
+        return true
+      }
+      return false
+      
+    }
   }
-  
-}
 
-// обработка ошибок
-function showError(item, error) {
-  
-  // обязательное поле не заполнено
-  if (item.validity.valueMissing) {
+  // обработка ошибок
+  function showError(item, error) {
     
-    if (item.getAttribute('data-input-title') == 'name') {
+    // обязательное поле не заполнено
+    if (item.validity.valueMissing) {
+      
+      if (item.getAttribute('data-input-title') == 'name') {
 
-      error.textContent = '— заполните поле «Имя»';
+        error.textContent = '— заполните поле «Имя»';
 
-    } else if (item.getAttribute('data-input-title') == 'sec-name') {
+      } else if (item.getAttribute('data-input-title') == 'sec-name') {
 
-      error.textContent = '— заполните поле «Фамилия»';
+        error.textContent = '— заполните поле «Фамилия»';
 
-    } else if (item.getAttribute('data-input-title') == 'address') {
+      } else if (item.getAttribute('data-input-title') == 'address') {
 
-      error.textContent = '— заполните поле «Адрес»';
+        error.textContent = '— заполните поле «Адрес»';
 
-    } else if (item.getAttribute('type') == 'tel' && !item.closest('.booking-popup-form')) {
+      } else if (item.getAttribute('type') == 'tel' && !item.closest('.booking-popup-form')) {
 
-      error.textContent = '— заполните поле «Телефон»';
+        error.textContent = '— заполните поле «Телефон»';
 
-    } else if (item.getAttribute('type') == 'email') {
+      } else if (item.getAttribute('type') == 'email') {
 
-      error.textContent = '— заполните поле «E-mail»';
+        error.textContent = '— заполните поле «E-mail»';
 
-    } else if (item.getAttribute('type') == 'checkbox' && !item.checked) {
+      } else if (item.getAttribute('type') == 'checkbox' && !item.checked) {
 
-      error.textContent = '— вы не можете продолжить без согласия с политикой конфиденциальности'
+        error.textContent = '— вы не можете продолжить без согласия с политикой конфиденциальности'
 
-    } else {
+      } else {
+        error.textContent = 'Обязательное поле';
+      }
+    
+    // введенный текст не соответствует типу инпута
+    } else if (item.validity.typeMismatch) {
+
+      if (item.getAttribute('type') == 'email') {
+
+        error.textContent = '— e-mail должен содержать символы «@», «.» проверьте правильность ввода';
+
+      } else {
+        error.textContent = 'Введено некорректно';
+      }
+
+    } else if (item.hasAttribute('data-readonly') && !item.value.length) {
       error.textContent = 'Обязательное поле';
-    }
-  
-  // введенный текст не соответствует типу инпута
-  } else if (item.validity.typeMismatch) {
+    } else if (item.getAttribute('type') == 'tel') {
 
-    if (item.getAttribute('type') == 'email') {
-
-      error.textContent = '— e-mail должен содержать символы «@», «.» проверьте правильность ввода';
+      error.textContent = 'Неверный формат';
 
     } else {
-      error.textContent = 'Введено некорректно';
+      // console.log('ne rabotaet');
     }
-
-  } else if (item.hasAttribute('data-readonly') && !item.value.length) {
-    error.textContent = 'Обязательное поле';
-  } else if (item.getAttribute('type') == 'tel') {
-
-    error.textContent = 'Неверный формат';
-
-  } else {
-    // console.log('ne rabotaet');
+    
+    // добавляет на ошибку активный класс
+    error.classList.add('active');
   }
-  
-  // добавляет на ошибку активный класс
-  error.className = 'form-error active';
+
+  // добавление элемента ошибки
+  function addingErrorBlock(item) {
+    if (item.getAttribute('type') == 'checkbox') {
+
+      item.closest('.checkbox').insertAdjacentHTML('beforeend', `<span class='form-error'></>`);
+      
+    } else if (item.closest('[data-select-input]')) {
+
+      item.closest('.select-label').insertAdjacentHTML('beforeend', `<span class='form-error'></>`);
+
+    } else if (item.hasAttribute('data-datepicker')) {
+
+      item.closest('.form__label').insertAdjacentHTML('beforeend', `<span class='form-error'></>`);
+
+    } else {
+      item.insertAdjacentHTML('afterend', `<span class='form-error'></>`)
+    }
+  }
+
+  // нахождение блока ошибки
+  function errorDefinition(item) {
+    if (item.getAttribute('type') == 'checkbox') {
+      error = item.closest('.checkbox').querySelector('.form-error');
+    } else if (item.closest('[data-select-input]')) {
+      error = item.closest('.select-label').querySelector('.form-error');
+    } else if (item.hasAttribute('data-datepicker')) {
+      error = item.closest('.form__label').querySelector('.form-error');
+    } else {
+      error = item.nextElementSibling;
+    }
+  }
 }
 
 export default formValidation;
